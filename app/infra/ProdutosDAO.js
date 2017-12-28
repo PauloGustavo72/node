9277b38042ef59cf5dac26a8f7572c3343343ed5
PauloGustavo72 <paulogustavo777@yahoo.com.br>
2017-12-28 11:24:20 -0200

    node aula3

---
+++ app/infra/ProdutosDAO.js
@@ -0,0 +1,18 @@
+function ProdutosDAO(connection) {
+    this._connection = connection;
+}
+
+ProdutosDAO.prototype.lista = function(callback) {
+    this._connection.query('select * from frases',callback);
+}
+
+
+ProdutosDAO.prototype.salva = function(produto, callback) {
+	
+    
+    this._connection.query('insert into frases set ?',produto, callback);
+}
+
+module.exports = function(){
+    return ProdutosDAO;
+};
---
+++ app/routes/produtos.js
@@ -0,0 +1,41 @@
+module.exports = function(app){
+
+    app.get('/produtos',function(req,res){
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.lista(function(erros,resultados){
+            res.json(resultados);
+        });
+
+        connection.end();
+    });
+
+    
+
+
+
+    app.get('/produtos/form', function(req, res){
+    	res.render('produtos/form');
+    });
+
+
+    
+
+   
+    app.post('/produtos',function(req,res){
+
+    var produto = req.body;
+
+    console.log(produto);
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+    
+
+    produtosDAO.salva(produto,function(erros,resultados){
+        res.redirect('/produtos');
+        });
+    });
+
+}
